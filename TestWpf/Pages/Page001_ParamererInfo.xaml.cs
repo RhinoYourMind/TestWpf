5f9e53fe0da2158fdc5685418e17a95f917846fb
751942324 <751942324@qq.com>
2018-09-03 20:56:06 +0800

    添加项目文件。

---
+++ TestWpf/Pages/Page001_ParamererInfo.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestWpf.Pages
+{
+
+    public partial class Page001_ParamererInfo : Page
+    {
+        public Page001_ParamererInfo()
+        {
+            InitializeComponent();
+        }
+    }
+}
